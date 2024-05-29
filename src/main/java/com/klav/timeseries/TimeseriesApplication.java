c334ca1625577b36a0f39037c91948a2ca9059c2
Valk Castellani <valkcastellani@gmail.com>
2024-05-28 21:01:47 -0300

    Spring Initializer Project

---
+++ src/main/java/com/klav/timeseries/TimeseriesApplication.java
@@ -0,0 +1,13 @@
+package com.klav.timeseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeseriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeseriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klav/timeseries/TimeseriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klav.timeseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeseriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
